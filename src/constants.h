5df56eed1b1c2bd598c1831693dbcbd1eb89073b
Nils Lenaerts <nils.lenaerts00@gmail.com>
2022-08-15 00:49:36 +0200

    added library for project wide constants

---
+++ src/constants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace constants{
+    // wat identifiers
+    std::vector<char> WAT_IDENTIFIERS = std::vector<char>{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '/', ':', '<', '=', '>', '?', '@', '\\', '^', '_', '`', '|', };
+}
