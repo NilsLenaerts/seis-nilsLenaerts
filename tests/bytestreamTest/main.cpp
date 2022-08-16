fb45881f402d5130d08c25ebab86cde77250cf42
Nils Lenaerts <nils.lenaerts00@gmail.com>
2022-08-16 14:13:05 +0200

    testing of bytestream with some lexer tests

--- tests/bytestreamTest/main.cpp
+++ tests/bytestreamTest/main.cpp
@@ -2,8 +2,55 @@
 #include <iostream>
 #include <cstdint>
 
+
 int main() {
-    ByteStream bs("./number");
-    std::cout << bs.readInt32() << std::endl;
+    ByteStream bs("./string.txt");
+    std::string in{"hello with \"escape \n lalala\" en ook nog eens een tab \t over hier, 10\\5 = 2"};
+    std::string out{};
+    bs.seek(1);
+    while(bs.peekByte() != '\"'){
+        if (bs.peekByte() == '\\'){
+            bs.seek(1);
+            unsigned char escapedChar = bs.peekByte();
+            switch (escapedChar)
+            {
+            case '"':
+                out.append(1,'\"');
+                bs.seek(1);
+                break;
+            case 'n':
+                out.append(1,'\n');
+                bs.seek(1);
+                break;
+            case 't':
+                out.append(1,'\t');
+                bs.seek(1);
+                break;
+            case 'r':
+                out.append(1,'\r');
+                bs.seek(1);
+                break;
+            case '\'':
+                out.append(1,'\'');
+                bs.seek(1);
+                break;
+            case '\\':
+                out.append(1,'\\');
+                bs.seek(1);
+                break;
+            default:
+                out.append(1,'\\');
+                out.append(1,bs.readByte());
+                break;
+            }
+            
+        }else{
+            out.append(1,bs.readByte());
+        }
+        
+    }
+    std::cout << in << std::endl;
+    std::cout << out;
+    
     return 0;
 }
